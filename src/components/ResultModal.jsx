ee3334494de75406fcb681ecc8445647797c5291
Keerthi Gurivelli <keerthigurivellio5@gmail.com>
2025-07-03 00:57:20 +0530

    second commit:updated win

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -2,10 +2,13 @@ import {createPortal} from 'react-dom';
 export default function ResultModal({ref,remainingTime,targetTime,onReset}){
     const userLost = remainingTime <=0 ;
     const formattedRemTime = (remainingTime/1000).toFixed(2);
+    const stoppedTime = targetTime - remainingTime/1000;
+    const winMargin = 0.1; // seconds
+    const userWin = !userLost && Math.abs(stoppedTime) <= winMargin;
     const score = Math.max(0,(1-(remainingTime/1000)/targetTime).toFixed(2)*100);
     return createPortal(
         <dialog  ref={ref} className="result-modal" >
-            <h2> {userLost ?"You Lost":score}</h2>
+            <h2> {userLost ? "You Lost" : userWin ? "You Win!" : score}</h2>
             <p>the target time was <strong>{targetTime} seconds. </strong></p>
             <p>you stopped the timer with <strong>{formattedRemTime} seconds left</strong> </p>
             <form method="dialog" onSubmit={onReset}>
